7b9e0fa62cdac2b53e3593f879d560590d2033d2
manlai <manlai@mezorn.com>
2017-05-27 22:58:57 +0900

    mezorn message

---
+++ app/src/main/java/com/mezorn/burtgel_2017/models/User.java
@@ -0,0 +1,23 @@
+package com.mezorn.burtgel_2017.models;
+
+import io.realm.RealmObject;
+import io.realm.annotations.RealmClass;
+
+/**
+ * Created by manlai on 5/27/17.
+ */
+@RealmClass
+public class User extends RealmObject {
+
+    private String          name;
+    private int             age;
+    private String gender;
+    private String register;
+    private String address;
+
+
+
+
+
+
+}
